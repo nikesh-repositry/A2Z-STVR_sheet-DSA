40b8e742d69e17a2be28796ee10ba494d27041f7
NIKESH <nikeshnoida@gmail.com>
2025-08-15 23:06:28 +0530

    Add files via upload

---
+++ strings/easy/problem_1.java
@@ -0,0 +1,16 @@
+//print all the substring of a given string
+package ONE_STRINGS;
+import java.util.Scanner;
+public class problem_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the string please:  ");
+        String s = input.nextLine();
+        System.out.println("String length is:  "+s.length());
+        for(int i = 0; i<s.length();i++){
+            for(int j = i+1; j<=s.length();j++){
+                System.out.print(s.substring(i,j)+" ");
+            }
+        }
+    }
+}
