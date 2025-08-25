c1d3db2e1328cad6fabe43bf0313f5ee6ae5fb80
NIKESH <nikeshnoida@gmail.com>
2025-08-25 19:49:07 +0530

    Update problem_1.java
    print all the substring of the string

--- strings/easy/problem_1.java
+++ strings/easy/problem_1.java
@@ -7,10 +7,15 @@ public class problem_1 {
         System.out.print("enter the string please:  ");
         String s = input.nextLine();
         System.out.println("String length is:  "+s.length());
+        int count = 0;
         for(int i = 0; i<s.length();i++){
             for(int j = i+1; j<=s.length();j++){
                 System.out.print(s.substring(i,j)+" ");
+                count+=1;
             }
+//            System.out.println();   if this execute then a pattern of name will be printed
         }
+        System.out.println();
+        System.out.print("total no of substring is "+count);
     }
 }
